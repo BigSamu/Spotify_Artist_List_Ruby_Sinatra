c5395f214d4f6c60acf7c6225a67cc79ad32bcca
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-06-21 22:50:03 -0400

    adding prompt for credentials

--- config/auth.rb
+++ config/auth.rb
@@ -5,6 +5,10 @@ require_relative 'settings'         # Import the settings module
 module Auth
   class Token
     def self.get_access_token
+      if Settings::CLIENT_ID.nil? || Settings::CLIENT_SECRET.nil?
+        prompt_user_for_credentials()
+      end
+
       response = RestClient.post(Settings::SPOTIFY_AUTH_URL, {
         grant_type: 'client_credentials',
         client_id: Settings::CLIENT_ID,
@@ -17,6 +21,21 @@ module Auth
       else
         # Handle the case when obtaining the access token fails
         nil
+      end
+    end
+
+    private
+
+    def self.prompt_user_for_credentials
+      print "Enter your Spotify API client ID: "
+      client_id = $stdin.gets.chomp
+
+      print "Enter your Spotify API client secret: "
+      client_secret = $stdin.gets.chomp
+
+      Settings.module_eval do
+        const_set('CLIENT_ID', client_id)
+        const_set('CLIENT_SECRET', client_secret)
       end
     end
   end
